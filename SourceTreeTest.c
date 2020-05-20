93e36661905fcc0577f8db42977341b7f83fbe80
brightdragom <brightdragom@gmail.com>
2020-05-20 10:07:28 +0900

    Test브랜치에 코드를 추가함

--- SourceTreeTest.c
+++ SourceTreeTest.c
@@ -1,7 +1,13 @@
 #include<stdio.h>
 
 int main(){
+  int i = 0;
+
   printf("Frist OSS Code\n");
   printf("Second OSS Code\n");
+
+  for(int i = 0 ; i < 10; i++){
+    print("Test 브랜치\n");
+  }
   return 0;
 }
